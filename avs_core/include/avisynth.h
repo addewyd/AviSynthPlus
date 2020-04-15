95fec76e2e71176a1788988734782ba97d0f556e
Pinterf <pinterfer@gmail.com>
2020-04-15 18:27:31 +0200

    bump AVISYNTH_INTERFACE_VERSION to 8

--- avs_core/include/avisynth.h
+++ avs_core/include/avisynth.h
@@ -55,8 +55,8 @@
 // graphical user interfaces.
 
 
-#ifndef __AVISYNTH_6_H__
-#define __AVISYNTH_6_H__
+#ifndef __AVISYNTH_8_H__
+#define __AVISYNTH_8_H__
 
 #include "avs/config.h"
 #include "avs/capi.h"
@@ -71,7 +71,7 @@
 #define __cdecl
 #endif
 
-enum { AVISYNTH_INTERFACE_VERSION = 6 };
+enum { AVISYNTH_INTERFACE_VERSION = 8 };
 
 
 /* Compiler-specific crap */
@@ -1921,4 +1921,4 @@ AVSC_API(IScriptEnvironment2*, CreateScriptEnvironment2)(int version = AVISYNTH_
 
 #pragma pack(pop)
 
-#endif //__AVISYNTH_6_H__
+#endif //__AVISYNTH_8_H__
